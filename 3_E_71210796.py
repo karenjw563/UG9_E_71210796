7eb820bf62c513d3cc17a5c0f94a26c611541cb4
karen <karen.jeanira@ti.ukdw.ac.id>
2021-11-12 11:04:10 +0700

    3_E_71210796.py

---
+++ 3_E_71210796.py
@@ -0,0 +1,9 @@
+#Fitur untuk menambah catatan di program Dani
+a = input("Masukkan daftar belanja 1: ")
+b = input("Masukkan daftar belanja 2: ")
+c = input("Masukkan data ke daftar belanja 1: ")
+d = input("Masukkan data ke daftar belanja 2: ")
+print ("Daftar belanja 1 adalah" ,a +',',end="")
+print (c)
+print ("Daftar belanja 2 adalah" ,b +',',end="")
+print (d)
